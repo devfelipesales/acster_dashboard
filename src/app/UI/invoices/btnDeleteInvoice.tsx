25c9561f6e70c20c08c4d28ffc3a311a5a22e951
Felipe Sales <115935636+devfelipesales@users.noreply.github.com>
2023-11-21 22:05:53 -0300

    style: Edited icon and text on card delete invoice

--- src/app/UI/invoices/btnDeleteInvoice.tsx
+++ src/app/UI/invoices/btnDeleteInvoice.tsx
@@ -47,10 +47,28 @@ export default function BtnDelete({
         <Modal.Header />
         <Modal.Body>
           <div className="text-center">
-            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 " />
-            <h3 className="mb-5 text-lg font-normal text-gray-500 ">
-              Tem certeza que deseja deletar essa fatura ?
-            </h3>
+            <div className="flex w-full justify-center pb-3">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
+                <svg
+                  className="h-6 w-6 text-red-600"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <h3 className="mb-3 font-semibold ">Exclusão de Fatura</h3>
+            <p className="mb-5 font-normal text-gray-600 ">
+              Tem certeza que deseja deletar essa fatura de forma permanente?
+            </p>
 
             <div className="flex items-center justify-between border p-4">
               <div className="">
