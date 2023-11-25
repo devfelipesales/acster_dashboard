11d30b4bc60994113643be7e7ffa6c1ddcc61376
Felipe Sales <115935636+devfelipesales@users.noreply.github.com>
2023-11-25 16:53:26 -0300

    feat: added breadcrumbs

---
+++ src/app/UI/invoices/breadcrumbs.tsx
@@ -0,0 +1,55 @@
+import { clsx } from "clsx";
+import Link from "next/link";
+
+interface Breadcrumb {
+  label: string;
+  href: string;
+  active?: boolean;
+}
+
+export default function Breadcrumbs({
+  breadcrumbs,
+}: {
+  breadcrumbs: Breadcrumb[];
+}) {
+  return (
+    <nav
+      aria-label="Breadcrumb"
+      className={`block max-w-fit rounded-lg border border-gray-200 bg-gray-50 px-2 py-3 text-gray-700`}
+    >
+      <ol className="flex text-base">
+        {breadcrumbs.map((breadcrumb, index) => (
+          <li
+            key={breadcrumb.href}
+            aria-current={breadcrumb.active}
+            className={clsx(
+              breadcrumb.active ? "text-gray-900" : "text-gray-500",
+            )}
+          >
+            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+            {index < breadcrumbs.length - 1 ? (
+              <span className="inline-block">
+                {" "}
+                <svg
+                  className="mx-1 block h-2 w-2 text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>{" "}
+              </span>
+            ) : null}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
