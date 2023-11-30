524221b9df77b90dd0d65e804208354d6423bfc1
Felipe Sales <115935636+devfelipesales@users.noreply.github.com>
2023-11-29 22:58:34 -0300

    fix: formatDate

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -6,12 +6,23 @@ export const formatCurrency = (amount: number) => {
 };
 
 export const formatDateToLocal = (date: Date, locale: string = "pt-BR") => {
-  return date.toLocaleDateString(locale);
+  // console.log(date);
+
+  // console.log(date.toISOString());
+  // console.log(date.toUTCString());
+  // console.log(date.setHours(date.getHours() - 3));
+  // console.log(date.toISOString());
+  // console.log(date.toUTCString());
+
+  // console.log("------");
+
+  return date.toLocaleDateString("pt-BR", { timeZone: "America/Sao_Paulo" });
 
   // const options: Intl.DateTimeFormatOptions = {
   //   day: "numeric",
   //   month: "numeric",
   //   year: "2-digit",
+  // timeZone: 'America/Sao_Paulo',
   // };
 
   // const formatter = new Intl.DateTimeFormat(locale, options);
