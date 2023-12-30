2c67d4d9a5cdd25a6d905ede23ddbaedc083a297
Felipe Sales <115935636+devfelipesales@users.noreply.github.com>
2023-12-29 21:19:09 -0300

    Update utils.ts
    remove comments

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -6,25 +6,9 @@ export const formatCurrency = (amount: number) => {
 };
 
 export const formatDateToLocal = (date: Date, locale: string = "pt-BR") => {
-  // console.log(date);
 
-  // console.log(date.toISOString());
-  // console.log(date.toUTCString());
-  // console.log(date.setHours(date.getHours() - 3));
-  // console.log(date.toISOString());
-  // console.log(date.toUTCString());
 
-  // console.log("------");
+return date.toLocaleDateString(locale, { timeZone: "America/Sao_Paulo" });
 
-  return date.toLocaleDateString("pt-BR", { timeZone: "America/Sao_Paulo" });
 
-  // const options: Intl.DateTimeFormatOptions = {
-  //   day: "numeric",
-  //   month: "numeric",
-  //   year: "2-digit",
-  // timeZone: 'America/Sao_Paulo',
-  // };
-
-  // const formatter = new Intl.DateTimeFormat(locale, options);
-  // return formatter.format(date);
 };
